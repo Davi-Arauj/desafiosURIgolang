6b12f029f8c5a7250aeb2660d03510db53992933
Davi-Araujo <moreiradaavi@gmail.com>
2022-05-10 14:50:01 -0300

    Salário com Bônus(1009)

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	
-	var cod1,num1,cod2,num2 int64
-	var valor1,valor2 float64
+	var salario,vendas float64
+	var nome string
 
-	fmt.Scanf("%d %d %f",&cod1,&num1,&valor1)
-	fmt.Scanf("%d %d %f",&cod2,&num2,&valor2)
+	fmt.Scanf("%s",&nome)
+	fmt.Scanf("%f",&salario)
+	fmt.Scanf("%f",&vendas)
 
-	total := (valor1 * float64(num1)) + (valor2 * float64(num2))
+	comissao := (vendas*0.15)+salario
 
-	fmt.Printf("VALOR A PAGAR: R$ %.2f\n", total)
-
+	fmt.Printf("TOTAL = R$ %.2f\n",comissao)
 }
