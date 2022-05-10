7a3b1ab1a9ef5564f9f778dca92ee5aebd4f3c35
Davi-Araujo <moreiradaavi@gmail.com>
2022-05-10 16:05:46 -0300

    Área(1012)

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main() {
-	var raio int64
+	var a, b, c float64
 	pi := 3.14159
-	
-	fmt.Scanf("%d",&raio)
-	volume := ((4/3.0)*pi) *(math.Pow(float64(raio),3))
+	fmt.Scanf("%f %f %f", &a, &b, &c)
+	triangulo := (a * c) / 2
+	circulo := pi * (math.Pow(c, 2))
+	trapezio := ((a + b) * c) / 2
+	quadrado := math.Pow(b, 2)
+	retangulo := (a*b)
 
-	fmt.Printf("VOLUME = %.3f\n",volume)
+	fmt.Printf("TRIANGULO: %.3f\n", triangulo)
+	fmt.Printf("CIRCULO: %.3f\n", circulo)
+	fmt.Printf("TRAPEZIO: %.3f\n", trapezio)
+	fmt.Printf("QUADRADO: %.3f\n", quadrado)
+	fmt.Printf("RETANGULO: %.3f\n", retangulo)
+
+
 }
