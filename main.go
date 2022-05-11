df27f4d2547a306b6557e42825d3ce7f5e173c98
Davi-Araujo <moreiradaavi@gmail.com>
2022-05-11 10:31:08 -0300

    Distância(1016)

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	var x1, y1, x2, y2 float64
+	var x1 int64
 
-	fmt.Scanf("%f %f", &x1, &y1)
-	fmt.Scanf("%f %f", &x2, &y2)
-
-	distancia := math.Sqrt(math.Pow((x2-x1), 2) + math.Pow((y2-y1), 2))
-
-	fmt.Printf("%.4f\n", distancia)
+	fmt.Scanf("%d", &x1)
+	minutos := x1 * 2
+	fmt.Printf("%d minutos\n", minutos)
 
 }
