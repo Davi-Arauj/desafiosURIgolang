8033220fd0bb99a434d094b4f7abf11f0b4950a8
Davi-Araujo <moreiradaavi@gmail.com>
2022-05-12 13:52:50 -0300

    Conversão de Tempo(1019)

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import (
 )
 
 func main() {
-	var valor int64
-	var notas = [7]int64{100, 50, 20, 10, 5, 2, 1}
-	fmt.Scanf("%d", &valor)
-	fmt.Println(valor)
-
-	for _, nota := range notas {
-		qtdNotas := (valor / nota)
-		fmt.Printf("%d nota(s) de R$ %d,00\n", qtdNotas, nota)
-		valor = valor % nota
+	var segundos int64
+	fmt.Scanf("%d", &segundos)
+	
+	minutos := segundos / 60
+	horas := minutos /60
+	for minutos>60{
+		minutos = minutos -60
 	}
+	
+	fmt.Printf("%d:%d:%d\n", horas, minutos, segundos%60)
 
 }
